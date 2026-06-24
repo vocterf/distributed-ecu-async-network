ee882c8b3df0c3020f7c0c887c9e3727083d6fe0
vocterf <vocterr07@gmail.com>
2026-06-24 14:11:26 +0200

    feat: implement basic tokio timeout verification loop

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,33 @@
 use std::time::Duration;
-use tokio::sync::mpsc;
-use tokio::time::sleep;
+use tokio::time::{sleep, timeout};
 
-struct TyrePayload {
-    id: String,
-    pressure: f32,
+
+async fn request_ecu_status(processing_time_ms: u64) -> String {
+    println!("[ECU]: Request received.");
+
+    sleep(Duration::from_millis(processing_time_ms)).await;
+
+    String::from("ECU_STATUS: ACTIVE, NO_ERRORS")
 }
+
+
 
 #[tokio::main]
 async fn main() {
-    let (tx_gateway, mut rx_dashboard) = mpsc::channel::<TyrePayload>(4);
 
-    let tx_fl_sensor = tx_gateway.clone();
-    let tx_fr_sensor = tx_gateway.clone();
-    let tx_rl_sensor = tx_gateway.clone();
-    let tx_rr_sensor = tx_gateway.clone();
+    let ecu_delay = 1500;
 
-    tokio::spawn(async move {
-        loop {
-            let frame = TyrePayload {
-                id: String::from("Front Left"),
-                pressure: 2.2,
-            };
-            if tx_fl_sensor.send(frame).await.is_err() {
-                println!("fl channel is closed");
-                break;
-            }
-            sleep(Duration::from_millis(500)).await;
+    println!("Sending request... It has 1000ms to respond.");
+
+    let diagnostic_result = timeout(Duration::from_millis(1000), request_ecu_status(ecu_delay)).await ;
+
+    match diagnostic_result {
+        Ok(ecu_message) => {
+            println!("\n[SUCCESS]: Response in time.");
+            println!("[DASHBOARD]: Data: -> {}", ecu_message);
         }
-    });
-
-    tokio::spawn(async move {
-        loop {
-            let frame = TyrePayload {
-                id: String::from("Front Right"),
-                pressure: 2.2,
-            };
-            if tx_fr_sensor.send(frame).await.is_err() {
-                println!("fr channel is closed");
-                break;
-            }
-            sleep(Duration::from_millis(500)).await;
+        Err(_) => {
+            println!("\n[TIMEOUT CRITICAL]: Timeout.");
         }
-    });
-
-    tokio::spawn(async move {
-        loop {
-            let frame = TyrePayload {
-                id: String::from("Rear Left"),
-                pressure: 2.2,
-            };
-            if tx_rl_sensor.send(frame).await.is_err() {
-                println!("rl channel is closed");
-                break;
-            }
-            sleep(Duration::from_millis(500)).await;
-        }
-    });
-
-    tokio::spawn(async move {
-        let mut pressure = 2.2;
-        loop {
-            let frame = TyrePayload {
-                id: String::from("Rear Right"),
-                pressure,
-            };
-            if tx_rr_sensor.send(frame).await.is_err() {
-                println!("rr channel is closed");
-                break;
-            }
-            pressure -= 0.1;
-            if pressure <= 0.1 {
-                pressure = 2.2
-            }
-            sleep(Duration::from_millis(500)).await;
-        }
-    });
-
-    drop(tx_gateway);
-
-    while let Some(frame) = rx_dashboard.recv().await {
-        println!("{}: {:.2}", frame.id, frame.pressure);
     }
 }
