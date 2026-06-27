15755986c34c81205c0d00b3049e025407b2c435
vocterf <vocterr07@gmail.com>
2026-06-27 20:52:46 +0200

    feat(main): verify async connectivity

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,11 @@ use socketcan::{CanFrame, EmbeddedFrame, StandardId};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let socket = CanSocket::open("vcan0")?;
     
-    // Construct a standard CAN frame with ID 0x120 and payload [1, 2, 3, 4]
     let id = StandardId::new(0x120).unwrap();
     let frame = CanFrame::new(id, &[1, 2, 3, 4]).unwrap();
     
-    // Test write
     socket.write_frame(frame).await?;
     
-    // Test read
     let recv_frame = socket.read_frame().await?;
     println!("Recv: {:?}", recv_frame);
     
