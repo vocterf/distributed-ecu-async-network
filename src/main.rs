3bd3b565b4b9381d0a5624362f49c6c9edbbeb4f
vocterf <vocterr07@gmail.com>
2026-06-27 17:58:39 +0200

    feat: separate project into individual binaries

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,20 @@
-use std::time::Duration;
-use tokio::sync::mpsc;
-use tokio::time::sleep;
+use socketcan::tokio::CanSocket;
+use socketcan::{CanFrame, EmbeddedFrame, StandardId};
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    println!("=== ASYNCHRONOUS CHANNELS SIMULATION START ===");
-
-    let (tx, mut rx) = mpsc::channel::<u32>(5);
-
-    let tx_sensor_a = tx.clone();
-    let tx_sensor_b = tx; 
-
-    tokio::spawn(async move {
-        let simulated_speeds = [30, 32, 35];
-        for speed in simulated_speeds {
-            println!("[Sensor A: Wheel Speed] Emitting: {} km/h", speed);
-            
-            if tx_sensor_a.send(speed).await.is_err() {
-                eprintln!("[Sensor A] Channel closed by receiver. Terminating task.");
-                break;
-            }
-            sleep(Duration::from_millis(40)).await;
-        }
-    });
-
-    tokio::spawn(async move {
-        let simulated_pressures = [150, 200];
-        for pressure in simulated_pressures {
-            println!("  [Sensor B: Brake Fluid] Emitting: {} kPa", pressure);
-            
-            if tx_sensor_b.send(pressure).await.is_err() {
-                eprintln!("  [Sensor B] Channel closed by receiver. Terminating task.");
-                break;
-            }
-            sleep(Duration::from_millis(60)).await;
-        }
-    });
-
-    println!("[ECU Core] Initializing consumer event loop...");
-
-    while let Some(bus_packet) = rx.recv().await {
-        println!("[ECU Core] Bus Intercept -> Processing data packet: {}", bus_packet);
-    }
-
-    println!("=== ASYNCHRONOUS CHANNELS SIMULATION END ===");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let socket = CanSocket::open("vcan0")?;
+    
+    // Construct a standard CAN frame with ID 0x120 and payload [1, 2, 3, 4]
+    let id = StandardId::new(0x120).unwrap();
+    let frame = CanFrame::new(id, &[1, 2, 3, 4]).unwrap();
+    
+    // Test write
+    socket.write_frame(frame).await?;
+    
+    // Test read
+    let recv_frame = socket.read_frame().await?;
+    println!("Recv: {:?}", recv_frame);
+    
+    Ok(())
 }
